59b8bdc840d83caa494dc1a5fa547c6c0f9ae678
yuval <yuvalnsn@gmail.com>
2022-11-04 12:00:39 +0200

    Initial commit. Initial django app. The app called interface, and this will be the only app in this project for the moment.

---
+++ Comunication_LTD/Comunication_LTD/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Comunication_LTD project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Comunication_LTD.settings')
+
+application = get_asgi_application()
