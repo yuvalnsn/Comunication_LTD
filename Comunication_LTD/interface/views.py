79abbd7e028ed612b3f66bd3a1bbcebf1c0b0d0e
Daniel Aviv <aviv.daniel1999@gmail.com>
2022-12-14 14:03:32 +0200

    Adding unsecured code for registerNewCustomer, register, views. TODO: implement unsecured code also in login2 view Note* login should compare the plain text only.

--- Comunication_LTD/interface/views.py
+++ Comunication_LTD/interface/views.py
@@ -6,6 +6,10 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from interface.models import CustomUser, Customer
 from django.conf import settings
+from config import sec_lvl
+
+from django.db import connection
+import datetime
 
 
 def dashboard(request):
@@ -16,22 +20,49 @@ def dashboard(request):
 
 def login2(request):
     if request.method == "POST": # user is trying to signin
-        form = LoginForm(request.POST)
-        if not form.is_valid():
-            return render(request,"login.html", {'form': LoginForm()})
-        #   form is valid
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
+        if sec_lvl == 'high':
+            form = LoginForm(request.POST)
+            if not form.is_valid():
+                return render(request,"login.html", {'form': LoginForm()})
+            #   form is valid
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is None:
-            messages.error(request, "Password or username is incorrect")
-            return redirect('/interface/login')
+            if user is None:
+                messages.error(request, "Password or username is incorrect")
+                return redirect('/interface/login')
 
-        else: # username and password is correct
-            login(request, user)
-            return redirect('/interface/dashboard')
+            else: # username and password is correct
+                login(request, user)
+                return redirect('/interface/dashboard')
+        #TODO: fix implementation, pass username through the session (login(request, user))
+
+        # elif sec_lvl == 'low':
+        #     form = LoginForm(request.POST)
+        #     if not form.is_valid():
+        #         return render(request, "login.html", {'form': LoginForm()})
+        #     #   form is valid
+        #     username = form.cleaned_data['username']
+        #     password = form.cleaned_data['password']
+        #
+        #     # user = authenticate(request, username=username, password=password)
+        #     sqlQuery = "SELECT 1 FROM WHERE username = '%s' AND password = '%s'"
+        #     isValidUser = False
+        #
+        #     with connection.cursor() as cursor:
+        #         cursor.execute(sqlQuery)
+        #         isValidUser = len(cursor.fetchall()) > 0
+        #
+        #     # In case the password is not matching to the correct one
+        #     if not isValidUser:
+        #         messages.error(request, "Password or username is incorrect")
+        #         return redirect('/interface/login')
+        #
+        #     else:  # username and password is correct
+        #         login(request, user)
+        #         return redirect('/interface/dashboard')
 
     else: # [GET] loading login form
         return render(request,"login.html", {'form': LoginForm()})
@@ -47,28 +78,54 @@ def logout(request):
 
 def register(request):
     if request.method == "POST": # user is trying to signup
-        form = CustomUserCreationForm(request.POST)
-        if not form.is_valid():
-            return render(request, "register.html", {'form': form})
-    #     form is valid (password satisfies the conditions)
-        email = form.cleaned_data['email']
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password1']
+        if sec_lvl == 'high':
+            form = CustomUserCreationForm(request.POST)
+            if not form.is_valid():
+                return render(request, "register.html", {'form': form})
+        #     form is valid (password satisfies the conditions)
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
 
-        if CustomUser.objects.filter(username = username).exists():
-            messages.error(request, "Username is already exists")
+            if CustomUser.objects.filter(username = username).exists():
+                messages.error(request, "Username is already exists")
+                return redirect('/interface/login')
+
+            if CustomUser.objects.filter(email = email).exists():
+                messages.error(request, "Email already registered")
+                return redirect('/interface/login')
+
+        #     username and email are unique
+            user = CustomUser.objects.create_user(username=username, email=email, password=password)
+            user.save()
+        #     insert user into the db
+            messages.success(request, "Your account has been created.")
             return redirect('/interface/login')
+        elif sec_lvl == 'low':
+            form = CustomUserCreationForm(request.POST)
+            if not form.is_valid():
+                return render(request, "register.html", {'form': form})
+            #     form is valid (password satisfies the conditions)
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
 
-        if CustomUser.objects.filter(email = email).exists():
-            messages.error(request, "Email already registered")
+            if CustomUser.objects.filter(username=username).exists():
+                messages.error(request, "Username is already exists")
+                return redirect('/interface/login')
+
+            if CustomUser.objects.filter(email=email).exists():
+                messages.error(request, "Email already registered")
+                return redirect('/interface/login')
+
+            sqlQuery = f"INSERT INTO interface_customuser (is_superuser, first_name, last_name, is_staff, is_active, date_joined, username, email, password) VALUES (0, '', '', 0, 0, %s, '{username}', '{email}', '{password}')"
+            print(sqlQuery);
+
+            with connection.cursor() as cursor:
+                cursor.execute(sqlQuery, [datetime.datetime.now()])
+
+            messages.success(request, "Your account has been created.")
             return redirect('/interface/login')
-
-    #     username and email are unique
-        user = CustomUser.objects.create_user(username=username, email=email, password=password)
-        user.save()
-    #     insert user into the db
-        messages.success(request, "Your account has been created.")
-        return redirect('/interface/login')
 
     else: # [GET] loading register form
         form = CustomUserCreationForm()
@@ -80,17 +137,34 @@ def registerCustomer(request):
     form = registerCustomerForm()
 
     if request.method == "POST": # user is trying to signup
-        form = registerCustomerForm(request.POST)
-        if not form.is_valid():
-            return render(request, "register.html", {'form': form})
-        firstName = form.cleaned_data['firstName']
-        lastName = form.cleaned_data['lastName']
-        username = str(request.user)
-        print(username)
-        customer = Customer.objects.create(customerFirstName=firstName, customerLastName=lastName, username=username)
-        customer.save()
-        messages.success(request, "Your account has been created.")
-        return redirect('/interface/customers')
+        if sec_lvl == 'high':
+            form = registerCustomerForm(request.POST)
+            if not form.is_valid():
+                return render(request, "register.html", {'form': form})
+            firstName = form.cleaned_data['firstName']
+            lastName = form.cleaned_data['lastName']
+            username = str(request.user)
+            print(username)
+            customer = Customer.objects.create(customerFirstName=firstName, customerLastName=lastName,username=username)
+            customer.save()
+            messages.success(request, "Your account has been created.")
+            return redirect('/interface/customers')
+        elif sec_lvl == 'low':
+            form = registerCustomerForm(request.POST)
+            if not form.is_valid():
+                return render(request, "register.html", {'form': form})
+            firstName = form.cleaned_data['firstName']
+            lastName = form.cleaned_data['lastName']
+            username = str(request.user)
+
+            sqlQuery = "INSERT INTO project_hit.interface_customer (username, customerFirstName, customerLastName) VALUES ('%s', '%s', '%s')" % (username, firstName, lastName)
+            print(sqlQuery)
+
+            with connection.cursor() as cursor:
+                cursor.execute(sqlQuery)
+
+            messages.success(request, "Your account has been created.")
+            return redirect('/interface/customers')
     else:
         return render(request, "registerCustomer.html", {'form': form})
 
