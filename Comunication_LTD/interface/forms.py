2baf29552f8ced56a51bdcea37cb15a35a6a90d9
Daniel Aviv <aviv.daniel1999@gmail.com>
2022-12-25 14:25:42 +0200

    Updating UI. Current bugs: 1. register warning messages not working properly 2. we have new gmail key in order to send email TODO: instead going to the default page after several login attempts, generate a message that says "The user have been blocked"

--- Comunication_LTD/interface/forms.py
+++ Comunication_LTD/interface/forms.py
@@ -15,13 +15,13 @@ def is_common_password(password: str):
 class LoginForm(forms.Form):
     username = forms.CharField(label=('Username'), widget=forms.TextInput(attrs={
         'placeholder':('Username'),
-        'class': "form-control fadeIn second m-2 shadow-sm",
+        'class': "form-control fadeIn second m-2 shadow-sm control-label",
         'onfocus': "this.placeholder=''",
-        'onblur': "this.placeholder='Username'"
+        'onblur': "this.placeholder='Username'",
     }))
     password = forms.CharField(label=('Password'), widget=forms.TextInput(attrs={
         'placeholder':('Password'),
-        'class': 'form-control fadeIn second m-2 shadow-sm',
+        'class': 'form-control fadeIn second m-2 shadow-sm control-label',
         'onfocus': "this.placeholder=''",
         'onblur': "this.placeholder='password'",
         'type': 'password',
@@ -29,12 +29,39 @@ class LoginForm(forms.Form):
 
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label=('Username'), widget=forms.TextInput(attrs={
+        'placeholder': ('Username'),
+        'class': "form-control fadeIn second m-2 shadow-sm control-label",
+        'onfocus': "this.placeholder=''",
+        'onblur': "this.placeholder='Username'",
+    }))
+    email = forms.CharField(label=('Email'), widget=forms.TextInput(attrs={
+        'placeholder': ('Email'),
+        'class': 'form-control fadeIn second m-2 shadow-sm control-label',
+        'onfocus': "this.placeholder=''",
+        'onblur': "this.placeholder='Email'",
+        'type': 'email',
+    }))
+    password1 = forms.CharField(label=('Password'), widget=forms.TextInput(attrs={
+        'placeholder': ('Password'),
+        'class': 'form-control fadeIn second m-2 shadow-sm control-label',
+        'onfocus': "this.placeholder=''",
+        'onblur': "this.placeholder='Password'",
+        'type': 'password',
+
+    }))
+    password2 = forms.CharField(label=('Password confirmation'), widget=forms.TextInput(attrs={
+        'placeholder': ('Password'),
+        'class': 'form-control fadeIn second m-2 shadow-sm control-label',
+        'onfocus': "this.placeholder=''",
+        'onblur': "this.placeholder='Password'",
+        'type': 'password',
+    }))
+
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['password1'].required = False
-        self.fields['password2'].required = False
-        # If one field gets autocompleted but not the other, our 'neither
-        # password or both password' validation will be triggered.
+        self.fields['password1'].required = True
+        self.fields['password2'].required = True
         self.fields['password1'].widget.attrs['autocomplete'] = 'off'
         self.fields['password2'].widget.attrs['autocomplete'] = 'off'
 
@@ -61,80 +88,3 @@ class registerCustomerForm(forms.Form):
             'onfocus': "this.placeholder=''",
             'onblur': "this.placeholder='lastName'"
         }))
-
-
-
-        ##############
-
-# class registerForm(forms.Form):
-#     email = forms.CharField(label=('email'), widget=forms.TextInput(attrs={
-#         'placeholder':('Email'),
-#         'class': "form-control fadeIn second m-2 shadow-sm",
-#         'onfocus': "this.placeholder=''",
-#         'onblur': "this.placeholder='Email'"
-#     }))
-#     username = forms.CharField(label=('username'), widget=forms.TextInput(attrs={
-#         'placeholder':('username'),
-#         'class': "form-control fadeIn second m-2 shadow-sm",
-#         'onfocus': "this.placeholder=''",
-#         'onblur': "this.placeholder='username'"
-#     }))
-#     password = forms.CharField(validators=[is_common_password],min_length=min_password_length,label=('password'), widget=forms.TextInput(attrs={
-#         'placeholder':('Password'),
-#         'class': 'form-control fadeIn second m-2 shadow-sm',
-#         'onfocus': "this.placeholder=''",
-#         'onblur': "this.placeholder='password'",
-#         'type': 'password',
-#         'pattern': password_pattern,
-#         'title': 'Use 8 or more characters with a mix of letters, numbers'
-#     }))
-#
-#
-#
-# class SetPasswordForm2(forms.Form):
-#     """
-#     A form that lets a user change set their password without entering the old
-#     password
-#     """
-#     error_messages = {
-#         'password_mismatch': ('The two password fields didn’t match.'),
-#     }
-#     new_password1 = forms.CharField(
-#         label=("New password"),min_length=min_password_length,
-#         validators=[is_common_password],
-#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-#         strip=False,
-#         #help_text=password_validators_help_text_html(),
-#     )
-#     new_password2 = forms.CharField(
-#         label=("New password confirmation"),
-#         min_length=min_password_length,
-#         strip=False,
-#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-#     )
-#
-#     def __init__(self, user, *args, **kwargs):
-#         self.user = user
-#         super().__init__(*args, **kwargs)
-#
-#     def clean_new_password2(self):
-#         password1 = self.cleaned_data.get('new_password1')
-#         password2 = self.cleaned_data.get('new_password2')
-#         print(self.user)
-#         if password1 and password2:
-#             if password1 != password2:
-#                 raise ValidationError(
-#                     self.error_messages['password_mismatch'],
-#                     code='password_mismatch',
-#                 )
-#         # validate_password(password2, self.user)
-#         return password2
-#
-#     def save(self, commit=True):
-#         password = self.cleaned_data["new_password1"]
-#         self.user.set_password(password)
-#         if commit:
-#             self.user.save()
-#         return self.user
-#
-# #
