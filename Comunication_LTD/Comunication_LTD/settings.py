ca6d5727da3330c0a83a419d9ac6c328b111bbd8
Daniel Aviv <aviv.daniel1999@gmail.com>
2022-12-25 10:11:17 +0200

    Look at the following tutorial https://timonweb.com/django/https-django-development-server-ssl-certificate/.
    All you need to do in order to gain ssl-tls secure connection :
    1. brew install mkcert
    2. make CA trusted in your os, by running:
       mkcert -install
    3. create your own private key and certificate:
       mkcert -cert-file cert.pem -key-file key.pem localhost 127.0.0.1
    4. pip install django-extensions Werkzeug,
       django-extensions already added to INSTALLED_APPS.
       there is no need to add it again.
    5. from now on this is how we run the server
    python manage.py runserver_plus --cert-file cert.pem --key-file key.pem

--- Comunication_LTD/Comunication_LTD/settings.py
+++ Comunication_LTD/Comunication_LTD/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'interface'
+    'interface',
+    'django-extensions'
 ]
 
 MIDDLEWARE = [
